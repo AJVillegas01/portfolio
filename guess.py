c50c7a53dc6a8a70282931ad70125aa8d4a10d31
AJVillegas01 <alvarojvv2001@gmail.com>
2023-11-06 19:19:17 +0100

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+
+guess = 0
+tries=0
+while guess != 6 and tries <8:
+  guess = int(input("Guess the number:  "))
+  tries += 1
+  print('Number of tries:', tries)
+if tries == 8:
+    print('You have exceded the number of tries')
+else: 
+   print("You got it!")
